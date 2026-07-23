c9f0c2ad48c33fc26309970ee0bc58159144a9aa
comercial <comercial@DESKTOP-LVECLNA.sellnet.local>
2026-07-23 17:03:38 -0300

    fix(topbar): import Link to avoid 'Link is not defined' during prerender

--- components/app-topbar.tsx
+++ components/app-topbar.tsx
@@ -1,5 +1,6 @@
-'use client'
+"use client"
 
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Bell, Search } from 'lucide-react'
 import { SidebarTrigger } from '@/components/ui/sidebar'
